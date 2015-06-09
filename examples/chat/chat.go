6716e6f4c8ef513e1f8e11a46f25e90eeae1c360
errantspark <errantspark@gmail.com>
2015-06-08 19:31:59 -0700

    notes

--- examples/chat/chat.go
+++ examples/chat/chat.go
@@ -13,8 +13,12 @@ func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Expected a .dem file as argument")
 	}
+	fmt.Println(os.Args)
 
+	//this for loop just loops over the args array discarding the first arg
+	//that's what the _ does, this lets you parse multiple games
 	for _, path := range os.Args[1:] {
+		//initializes parser variable for ease of use := is initialize weheres = updates
 		parser := yasha.ParserFromFile(path)
 		parser.OnSayText2 = func(tick int, obj *dota.CUserMsg_SayText2) {
 			fmt.Printf("%s - %07d | %s: %s\n", filepath.Base(path), tick, obj.GetPrefix(), obj.GetText())
